7462dee6301273dc6cb8b01f5f9887859e84fc4d
lxhao61 <lxhao61@gmail.com>
2023-11-20 04:03:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,6 +17,10 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 # 取消Lean大登陆密码
 sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-settings
 
+# 删除自带msd_lite源码
+rm -rf feeds/packages/net/msd_lite
+rm -rf package/feeds/packages/msd_lite
+
 # 拉取PassWall源码
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
 #git clone -b packages https://github.com/lxhao61/openwrt-passwall.git package/passwall/packages
@@ -29,8 +33,9 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luc
 #git checkout d1e618220a9a0a4b73d536101f452a2f4cf14861
 #cd -
 
-# 拉取luci-app-smartdns插件
-#git clone https://github.com/ximiTech/luci-app-msd_lite.git package/luci-app-msd_lite
+# 拉取msd_lite源码
+git clone https://github.com/ximiTech/msd_lite.git package/msd_lite
+git clone https://github.com/ximiTech/luci-app-msd_lite.git package/luci-app-msd_lite
 
 # 拉取ShadowSocksR Plus+源码
 #git clone -b master https://github.com/fw876/helloworld.git package/helloworld
