56f09e4e11a19276a1849d09fc72fd1677249649
lxhao61 <lxhao61@gmail.com>
2023-12-31 09:54:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,14 +19,14 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-# 取消helloworld源注释（ssr-plus）
+# 取消 helloworld 源注释（ssr-plus）
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# 添加xiaorouji大passwall源
+# 添加 xiaorouji 大 passwall 源
 #sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
 
-# 注释掉默认的packages源
+# 注释掉默认的 packages 源
 #sed -i 's/^\(.*packages\)/#&/' feeds.conf.default
 
-# 添加修改的packages源
+# 添加修改的 packages 源
 #sed -i '$a src-git packages https://github.com/lxhao61/packages.git' feeds.conf.default
