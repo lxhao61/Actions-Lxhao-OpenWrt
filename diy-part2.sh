6ac3d223c8245d3734f5ad497c1b82da4f7a0b90
lxhao61 <lxhao61@gmail.com>
2024-01-19 22:40:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -41,4 +41,4 @@ git clone https://github.com/ximiTech/msd_lite.git package/msd_lite
 git clone https://github.com/ximiTech/luci-app-msd_lite.git package/luci-app-msd_lite
 
 # 拉取 luci-app-tailscale 源码
-git clone https://github.com/selfcan/luci-app-tailscale.git package/feeds/luci/luci-app-tailscale
+#git clone https://github.com/selfcan/luci-app-tailscale.git package/feeds/luci/luci-app-tailscale
