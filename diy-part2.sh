82d5fd20b7f241b6f02cb19727b7534ecb95b530
lxhao61 <lxhao61@gmail.com>
2024-01-20 05:58:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,13 +21,15 @@ sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-sett
 rm -rf feeds/packages/net/msd_lite
 rm -rf package/feeds/packages/msd_lite
 
-# 拉取 PassWall 源码
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
-#cd package/passwall/packages
+# 拉取 passwall-packages 源码
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/feeds/packages/passwall
+#cd package/feeds/packages/passwall
 #git checkout c189a68728d6bb65d9fb4b47fdacea3ba970a624
 #cd -
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
-#cd package/passwall/luci
+
+# 拉取 luci-app-passwall 源码
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/feeds/luci/luci-app-passwall
+#cd package/feeds/luci/luci-app-passwall
 #git checkout d1e618220a9a0a4b73d536101f452a2f4cf14861
 #cd -
 
