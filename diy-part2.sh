a0b37c240cca3006f22ebec98bb4c85b2a6caf4d
lxhao61 <lxhao61@gmail.com>
2024-01-25 09:37:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,28 +17,55 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 # 取消 Lean 大登陆密码
 sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-settings
 
-# 删除自带 msd_lite 源码
+# 删除自带 msd_lite
 rm -rf feeds/packages/net/msd_lite
 rm -rf package/feeds/packages/msd_lite
 
-# 拉取 msd_lite 源码
-git clone https://github.com/ximiTech/msd_lite.git package/feeds/packages/msd_lite
-git clone https://github.com/ximiTech/luci-app-msd_lite.git package/feeds/luci/luci-app-msd_lite
+# 拉取 msd_lite
+git clone https://github.com/ximiTech/msd_lite.git package/msd_lite/msd_lite
+git clone https://github.com/ximiTech/luci-app-msd_lite.git package/msd_lite/luci-app-msd_lite
 
-# 拉取 passwall-packages 源码
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/feeds/packages/passwall
-#cd package/feeds/packages/passwall
+# 拉取 passwall-packages
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
+#cd package/passwall/packages
 #git checkout c189a68728d6bb65d9fb4b47fdacea3ba970a624
 #cd -
 
-# 拉取 luci-app-passwall 源码
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/feeds/luci/luci-app-passwall
-#cd package/feeds/luci/luci-app-passwall
+# 拉取 luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci-app-passwall
+#cd package/passwall/luci-app-passwall
 #git checkout d1e618220a9a0a4b73d536101f452a2f4cf14861
 #cd -
 
-# 拉取 ShadowSocksR Plus+ 源码
+# 拉取 ShadowSocksR Plus+
 #git clone -b master https://github.com/fw876/helloworld.git package/helloworld
 
-# 拉取 luci-app-tailscale 源码
-#git clone https://github.com/selfcan/luci-app-tailscale.git package/feeds/luci/luci-app-tailscale
+# 删除 passwall-packages 中 naiveproxy
+rm -rf package/passwall/packages/naiveproxy
+
+# 删除自带 tailscale
+rm -rf feeds/packages/net/tailscale
+
+# 筛选程序
+function merge_package(){
+    # 参数1是分支名,参数2是库地址。所有文件下载到指定路径。
+    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+    trap 'rm -rf "$tmpdir"' EXIT
+    branch="$1" curl="$2" target_dir="$3" && shift 3
+    rootdir="$PWD"
+    localdir="$target_dir"
+    [ -d "$localdir" ] || mkdir -p "$localdir"
+    tmpdir="$(mktemp -d)" || exit 1
+    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+    cd "$tmpdir"
+    git sparse-checkout init --cone
+    git sparse-checkout set "$@"
+    for folder in "$@"; do
+        mv -f "$folder" "$rootdir/$localdir"
+    done
+    cd "$rootdir"
+}
+# 提取 naiveproxy
+merge_package master https://github.com/immortalwrt/packages.git package/passwall/packages net/naiveproxy
+# 提取 tailscale
+merge_package main https://github.com/kenzok8/small-package.git feeds/packages/net tailscale
